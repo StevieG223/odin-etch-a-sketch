34581029bdca241e7cf6f161822498537eaf452c
StevieG223 <77689111+StevieG223@users.noreply.github.com>
2024-02-01 14:00:52 -0500

    add styling, event listeners

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const containerDiv = document.getElementById("flex-grid");
+
 for (let i=0; i < 16; i++){
-    let div = document.createElement("div");
-    div.setAttribute("id", "square-div");
-    containerDiv.appendChild(div);
+    let divRow = document.createElement("div");
+    divRow.setAttribute("id", "flex-row");
+    containerDiv.appendChild(divRow);
+    for (let i=0; i < 16; i++){
+        let div = document.createElement("div");
+        div.setAttribute("class", "square-div");
+        div.addEventListener("mouseover", ()=>{
+            div.classList.add("square-mouseover");
+        }
+        );
+        div.addEventListener("mouseout", ()=>{
+            div.classList.remove("square-mouseover");
+            }
+            );
+        divRow.appendChild(div);
+    }
 }
