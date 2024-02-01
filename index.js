ab56e1ebf2458fe902d8ef79ed9efb919b346c37
StevieG223 <77689111+StevieG223@users.noreply.github.com>
2024-02-01 16:36:29 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const containerDiv = document.getElementById("flex-grid");
 const updateButton = document.getElementById("update-button");
+const maxGridSize = 500;
 
 function createGrid(size=16){
-for (let i=0; i < size; i++){
-    let divRow = document.createElement("div");
-    divRow.setAttribute("id", "flex-row");
-    containerDiv.appendChild(divRow);
     for (let i=0; i < size; i++){
-        let div = document.createElement("div");
-        div.setAttribute("class", "square-div");
-        div.addEventListener("mouseover", ()=>{
-            div.classList.add("square-mouseover");
-        }
-        );
-        div.addEventListener("mouseout", ()=>{
-            div.classList.remove("square-mouseover");
+        let divRow = document.createElement("div");
+        divRow.setAttribute("id", "flex-row");
+        let squareSize = Math.round(maxGridSize / (size));
+        containerDiv.appendChild(divRow);
+        for (let i=0; i < size; i++){
+            let div = document.createElement("div");
+            div.setAttribute("class", "square-div");
+            div.setAttribute('style', `height: ${squareSize}px; width:${squareSize}px;`);
+            div.addEventListener("mouseover", ()=>{
+                div.classList.add("square-mouseover");
             }
             );
-        divRow.appendChild(div);
-    }   
-}
+            div.addEventListener("mouseout", ()=>{
+                div.classList.remove("square-mouseover");
+                }
+                );
+            divRow.appendChild(div);
+        }   
+    }
 }
 createGrid();
 updateButton.addEventListener("click", ()=>{
@@ -27,5 +30,3 @@ updateButton.addEventListener("click", ()=>{
     let gridSize = document.getElementById('grid-size').value;
     createGrid(gridSize);
 })
-
-
